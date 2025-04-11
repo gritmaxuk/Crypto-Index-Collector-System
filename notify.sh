c2668c45bd96be2d17d3378215ad4dcc92babf11
gritmax <empty@empty>
2025-04-11 23:13:40 +0100

    Refactoring

--- notify.sh
+++ notify.sh
@@ -10,19 +10,6 @@ MESSAGE="$1"
 # Log the notification
 echo "[$TIMESTAMP] $MESSAGE" >> "$LOG_FILE"
 
-# Uncomment and configure one of the following notification methods:
-
-# Email notification (requires mailx)
-# echo "$MESSAGE" | mail -s "Crypto Index Collector Alert" your-email@example.com
-
-# Slack notification (requires curl)
-# SLACK_WEBHOOK_URL="your-webhook-url"
-# curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$MESSAGE\"}" "$SLACK_WEBHOOK_URL"
-
-# Discord notification (requires curl)
-# DISCORD_WEBHOOK_URL="your-webhook-url"
-# curl -X POST -H "Content-Type: application/json" --data "{\"content\":\"$MESSAGE\"}" "$DISCORD_WEBHOOK_URL"
-
 # Print to console as well
 echo "[$TIMESTAMP] $MESSAGE"
 
